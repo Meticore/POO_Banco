f5c64ba21dac654bba4934532c990cc7c26d0c05
Ricardo Fonseca <ricardogremista26@gmail.com>
2015-04-22 19:47:58 -0300

    Alterações realizadas:
    -Feita a classe Message, que contém 6 mensagens;

---
+++ src/Messages.java
@@ -0,0 +1,31 @@
+
+public class Messages {
+
+	int option;
+	
+	public String getMensagem (int option){
+		this.option = option;
+		
+		switch (option){
+		
+		case 1:
+			return "Saque realizado com sucesso!";
+		case 2:
+			return "Erro! \nSaldo insuficiente.";			
+		case 3:
+			return "Transferencia realizada com sucesso!";
+		case 4:
+			return "Erro! \nTransferencia nao realizada, \nSaldo insuficiente";
+		case 5:
+			return "Deposito realizado com sucesso!";
+		case 6:
+			return "Erro! \nDeposito nao realizado, \nSaldo infuficiente";
+			
+		}
+		return "Erro desconhecido! \nFale com o gerente responsavel por sua conta!";
+	
+	}
+	
+	
+
+}
